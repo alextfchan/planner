67ea6451416d9003fdaa88f4ca026c837ae57fef
Alex Chan <alextfchan96@gmail.com>
2024-04-30 08:28:20 +0100

    created CSS files and updated project settings to include static files for CSS files for both project and specific apps (pomodoro and todolist).

--- plannerproject/settings.py
+++ plannerproject/settings.py
@@ -135,7 +135,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'plannerproject/static'),
+    os.path.join(BASE_DIR, 'app_pomodoro/static'),
+    os.path.join(BASE_DIR, 'apptodolist/static'),
+]
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
